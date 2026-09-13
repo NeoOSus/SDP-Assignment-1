39b9d87323420cd10f97403483f8917b04e115e5
Erkebulan <251288@astanait.edu.kz>
2026-09-13 18:43:50 +0500

    added second concrete Builder

---
+++ OfficePCBuilder.java
@@ -0,0 +1,32 @@
+public class OfficePCBuilder implements ComputerBuilder{
+    private Computer computer = new Computer();
+
+    @Override
+    public ComputerBuilder setCpu(String cpu) {
+        this.computer.setCpu(cpu);
+        return this;
+    }
+
+    @Override
+    public ComputerBuilder setGpu(String gpu) {
+        this.computer.setGpu(gpu);
+        return this;
+    }
+
+    @Override
+    public ComputerBuilder setRam(int ram) {
+        this.computer.setRam(ram);
+        return this;
+    }
+
+    @Override
+    public ComputerBuilder setStorage(int storage) {
+        this.computer.setStorage(storage);
+        return this;
+    }
+
+    @Override
+    public Computer build() {
+        return this.computer;
+    }
+}
